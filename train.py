6ab8d30cd9df3f74ece15c3a7845e8bec204ed6a
Chris <chris.blake@emiratesteamnz.com>
2020-11-20 16:07:44 +1300

    added comments on performance

--- train.py
+++ train.py
@@ -7,6 +7,10 @@ gym.envs.registration.register(
     entry_point=GameEnv
 )
 env = lambda: gym.make("prestart-v0")
-kwargs = dict(hidden_sizes=(128,256,256,256,128))
-ppo.ppo(env, ac_kwargs=kwargs, epochs=50, steps_per_epoch=20000, pi_lr=1e-4, vf_lr=1e-4, train_pi_iters=10, train_v_iters=10)
+kwargs = dict(hidden_sizes=(256,512,512,256))
+ppo.ppo(env, ac_kwargs=kwargs, epochs=3, steps_per_epoch=20000, pi_lr=3e-4, vf_lr=3e-4, train_pi_iters=10, train_v_iters=10)
 
+# in this configuration the julia version with no multithreading takes around 10s for the episode collection and 0.9s for the update (using the GPU)
+# the current version using spinningup and cython based env takes around 30s for episode collection and 15s for the update (not using the GPU)
+# from profiling the code, all the time is spent inside pytorch functions and can't be optimised much.
+# it looks like the julia version is a much better option.
